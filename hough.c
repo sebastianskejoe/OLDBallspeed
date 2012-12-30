f4c464c729fdf23d74234ca6e908623e94f1ac84
Sebastian Skejø <sebastianskejoe@gmail.com>
2012-12-30 14:24:48 +0100

    OpenCV is the only method for now

--- hough.c
+++ hough.c
@@ -3,11 +3,9 @@
 #include "stdio.h"
 
 HoughResult houghTransform() {
-    #if OPENCV_BACKEND
-        printf("Use openCV\n");
-        // Do opencv hough transform
-    #else
-        printf("Use own implementation\n");
-        // Implement own hough transform
-    #endif
+    HoughResult hr;
+    printf("Use openCV\n");
+    // Do opencv hough transform
+    
+    return hr;
 }
